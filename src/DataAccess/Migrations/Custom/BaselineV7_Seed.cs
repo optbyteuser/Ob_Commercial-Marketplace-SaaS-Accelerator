62187c292a4a481f7d4102348558cf32d6098a39
Sayali Shende <sayalis@optbyte.com>
2024-06-11 16:25:17 +0530

    changes

--- src/DataAccess/Migrations/Custom/BaselineV7_Seed.cs
+++ src/DataAccess/Migrations/Custom/BaselineV7_Seed.cs
@@ -14,7 +14,75 @@ namespace Marketplace.SaaS.Accelerator.DataAccess.Migrations.Custom
         {
             var seedDate = DateTime.Now;
             migrationBuilder.Sql(@$"
-                                Select * from public.""ApplicationConfiguration"";
+                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                SELECT 'WebNotificationUrl', '', 'Setting this URL will enable pushing LandingPage/Webhook events to this external URL'
+                                WHERE NOT EXISTS (
+                                SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'WebNotificationUrl'
+                                );
+
+                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                SELECT 'EnablesSuccessfulSchedulerEmail', 'False', 'This will enable sending email for successful metered usage.'
+                                WHERE NOT EXISTS (
+                                    SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'EnablesSuccessfulSchedulerEmail'
+                                );
+
+                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                SELECT 'EnablesFailureSchedulerEmail', 'False', 'This will enable sending email for failure metered usage.'
+                                WHERE NOT EXISTS (
+                                    SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'EnablesFailureSchedulerEmail'
+                                );
+
+                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                SELECT 'EnablesMissingSchedulerEmail', 'False', 'This will enable sending email for missing metered usage.'
+                                WHERE NOT EXISTS (
+                                    SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'EnablesMissingSchedulerEmail'
+                                );
+
+                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                SELECT 'SchedulerEmailTo', '', 'Scheduler email receiver(s)'
+                                WHERE NOT EXISTS (
+                                    SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'SchedulerEmailTo'
+                                );
+                  
+                                INSERT INTO ""EmailTemplate"" (""Status"", ""Description"", ""InsertDate"", ""TemplateBody"", ""Subject"", ""IsActive"")
+                                SELECT 'Accepted', 'Accepted', CURRENT_TIMESTAMP, 
+                                '<html> <head> <meta http-equiv=""Content-Type"" content=""text/html; charset=UTF-8""/> </head> 
+                                <body leftmargin=""0"" marginwidth=""0"" topmargin=""0"" marginheight=""0"" offset=""0""> 
+                                <center> <table align=""center"" border=""0"" cellpadding=""0"" cellspacing=""0"" height=""100%"" width=""100%"" id=""bodyTable""> 
+                                <tr><td align=""center"" valign=""top"" id=""bodyCell""><!-- BEGIN TEMPLATE // --> 
+                                <table border=""0"" cellpadding=""0"" cellspacing=""0"" id=""templateContainer""> 
+                                <tr><td align=""center"" valign=""top""><!-- BEGIN BODY // --> 
+                                <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" id=""templateBody""> 
+                                <tr><td valign=""top"" class=""bodyContent""><h2>Subscription ****SubscriptionName****</h2><br> 
+                                <p>The Scheduled Task ****SchedulerTaskName**** was fired <b>Successfully</b></p> 
+                                <p>The following section is the detail results.</p><hr/> 
+                                <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" id=""templateBody""> 
+                                ****ResponseJson**** </table></td></tr></table></td></tr></table><!-- // END BODY --></td></tr> </table> <!-- // END TEMPLATE --> 
+                                </center> </body> </html>', 'Scheduled SaaS Metered Usage Submitted Successfully!', true
+                                WHERE NOT EXISTS (
+                                    SELECT 1 FROM ""EmailTemplate"" WHERE ""Status"" = 'Accepted'
+                                );
+
+                                INSERT INTO ""EmailTemplate"" (""Status"", ""Description"", ""InsertDate"", ""TemplateBody"", ""Subject"", ""IsActive"")
+                                SELECT 'Failure', 'Failure', CURRENT_TIMESTAMP, 
+                                '<html><head><meta http-equiv=""Content-Type"" content=""text/html; charset=UTF-8""/></head> 
+                                <body leftmargin=""0"" marginwidth=""0"" topmargin=""0"" marginheight=""0"" offset=""0""> 
+                                <center><table align=""center"" border=""0"" cellpadding=""0"" cellspacing=""0"" height=""100%"" width=""100%"" id=""bodyTable""> 
+                                <tr><td align=""center"" valign=""top"" id=""bodyCell""><!-- BEGIN TEMPLATE // --> 
+                                <table border=""0"" cellpadding=""0"" cellspacing=""0"" id=""templateContainer""> 
+                                <tr><td align=""center"" valign=""top""><!-- BEGIN BODY // --> 
+                                <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" id=""templateBody""> 
+                                <tr> <td valign=""top"" class=""bodyContent""><h2 >Subscription ****SubscriptionName****</h2><br> 
+                                <p>The Scheduled Task ****SchedulerTaskName**** was fired<b> but Failed to Submit Data</b></p><br> 
+                                Please try again or contact technical support to troubleshoot the issue. 
+                                <p>The following section is the detail results.</p><hr/> 
+                                <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" id=""templateBody""> 
+                                ****ResponseJson****</table></td></tr></table></td> </tr></table><!-- // END BODY --></td></tr></table><!-- // END TEMPLATE --> 
+                                </center></body></html>', 'Scheduled Task Failed to Submit Data!', true
+                                WHERE NOT EXISTS (
+                                    SELECT 1 FROM ""EmailTemplate"" WHERE ""Status"" = 'Failure'
+                                );
+
                                 ");
         }
 
