392abcff271cf3ebab4dbe670bbbd25491f3d13e
Sayali Shende <sayalis@optbyte.com>
2024-06-11 20:07:22 +0530

    changes

--- src/DataAccess/Migrations/Custom/BaselineV7_Seed.cs
+++ src/DataAccess/Migrations/Custom/BaselineV7_Seed.cs
@@ -14,27 +14,27 @@ namespace Marketplace.SaaS.Accelerator.DataAccess.Migrations.Custom
         {
             var seedDate = DateTime.Now;
             migrationBuilder.Sql(@$"
-                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                INSERT INTO public.""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
                                 SELECT 'WebNotificationUrl', '', 'Setting this URL will enable pushing LandingPage/Webhook events to this external URL'
                                 WHERE NOT EXISTS (
                                 SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'WebNotificationUrl'
                                 );
-                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                INSERT INTO public.""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
                                 SELECT 'EnablesSuccessfulSchedulerEmail', 'False', 'This will enable sending email for successful metered usage.'
                                 WHERE NOT EXISTS (
                                     SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'EnablesSuccessfulSchedulerEmail'
                                 );
-                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                INSERT INTO public.""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
                                 SELECT 'EnablesFailureSchedulerEmail', 'False', 'This will enable sending email for failure metered usage.'
                                 WHERE NOT EXISTS (
                                     SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'EnablesFailureSchedulerEmail'
                                 );
-                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                INSERT INTO public.""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
                                 SELECT 'EnablesMissingSchedulerEmail', 'False', 'This will enable sending email for missing metered usage.'
                                 WHERE NOT EXISTS (
                                     SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'EnablesMissingSchedulerEmail'
                                 );
-                                INSERT INTO ""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
+                                INSERT INTO public.""ApplicationConfiguration"" (""Name"", ""Value"", ""Description"")
                                 SELECT 'SchedulerEmailTo', '', 'Scheduler email receiver(s)'
                                 WHERE NOT EXISTS (
                                     SELECT 1 FROM ""ApplicationConfiguration"" WHERE ""Name"" = 'SchedulerEmailTo'
